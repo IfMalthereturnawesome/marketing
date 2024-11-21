6579cf791a9976f04903e28d41698c70e76ed7e4
IfMalthereturnawesome <malt996b@stud.kea.dk>
2024-11-21 21:40:58 +0100

    Add new page background.

--- src/pages/profile-card-page.tsx
+++ src/pages/profile-card-page.tsx
@@ -3,11 +3,7 @@ import { profileCardData } from '@/data/profileCardData';
 
 const ProfileCardPage = () => {
   return (
-    <main
-      className={
-        'min-h-[100vh] bg-gradient-to-b from-gray-50 to-[#d2d6db] pt-[200px]'
-      }
-    >
+    <main className={'page-background min-h-[100vh] pt-[200px]'}>
       {profileCardData.map((profile) => (
         <ProfileCard key={profile.id} {...profile} />
       ))}
