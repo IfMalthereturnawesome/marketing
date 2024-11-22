e98222aa8a9edce3d1ae4149a4ec89dedce4bd87
IfMalthereturnawesome <malt996b@stud.kea.dk>
2024-11-22 21:17:02 +0100

    Fix image.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ type HeroSectionProps = HeroSection & {
 	className?: string;
 };
 
-const HeroSection = ({ title, description, imageBig, className }: HeroSectionProps) => {
+const HeroSection = ({ title, description, image, imageBig, className }: HeroSectionProps) => {
 	return (
 		<div
 			className={`flex w-full flex-1 flex-col justify-center rounded-md bg-neutral-subtle shadow-sm md:py-2 md:shadow-md
@@ -72,9 +72,9 @@ const HeroSection = ({ title, description, imageBig, className }: HeroSectionPro
 					</div>
 					<div className={'flex w-full items-center justify-center self-stretch rounded-lg'}>
 						<img
-							src={imageBig}
+							src={image}
 							alt={'Prism image'}
-							className={'object-cover md:hidden'}
+							className={'flex items-center self-stretch object-cover p-8 md:hidden'}
 						/>
 						<img
 							src={imageBig}
